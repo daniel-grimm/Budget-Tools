63c5a64e7ca9767743d1fe768de80d6a6f39ea69
daniel-grimm <danielsgrimm@hotmail.com>
2019-05-03 20:10:49 -0700

    Starting development of the application.

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,8 +4,13 @@
  * This is a library file for the Budget-Tools calculator.
  */
 
- public static class Calculator {
+ public class Calculator {
 
+    /**Returns what the amount multiplied by the given percent.
+     * @param amount : The amount to be multiplied by
+     * @param percent : The percent of the amount
+     * @return : The percentage of the amount
+     */
     public static double percentOf(double amount, double percent) {
         return amount * (percent / 100);
     }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
+
+    public static void run(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+
+        long endTime = System.currentTimeMillis();
+        long runtime = endTime - startTime;
+        runtime /= 1000;
+        System.out.println("\tRun time: " + runtime + " seconds");
+    }
     
     public static void main(String[] args) {
-        System.out.println(Calculator.percentOf(100, 35));
+        run(args);
     }
 }
