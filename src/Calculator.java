e445b0d9ad872c9bc8869b766cbf7617889da524
daniel-grimm <danielsgrimm@hotmail.com>
2019-05-03 19:32:06 -0700

    Ensuring development environment is working.

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+/**@author Daniel Grimm
+ * @version 0.0
+ * 
+ * This is a library file for the Budget-Tools calculator.
+ */
+
+ public static class Calculator {
+
+    public static double percentOf(double amount, double percent) {
+        return amount * (percent / 100);
+    }
+
+ }
