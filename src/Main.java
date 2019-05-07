98aab63f9e1c4740299bc33a2fe63c520b813655
daniel-grimm <danielsgrimm@hotmail.com>
2019-05-06 20:53:12 -0700

    Saving work for the evening.

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,101 @@
  * @version 0.0
  */
 
-import java.io.*;           //For file I/O
-import java.util.Scanner;   //For reading in from files
+import java.io.File;                    //For file I/O
+import java.io.FileNotFoundException;   //File I/O
+import java.util.ArrayList;             //Flexible data
+import java.util.Scanner;               //For reading in from files
 
 public class Main {
 
-    public static void printFile() {
+    public static void displayInfo() {
         //
     }
 
-    public static void printExpenses() {
-        //
+    /**Prints the content of the input file to the console.
+     * 
+     * @param title
+     * @param grossIncome
+     * @param taxBracket
+     * @param tithe
+     * @param rent
+     * @param food
+     * @param gas
+     */
+    public static void printFile(String title, int grossIncome, int taxBracket, int tithe, int rent, int food, int gas) {
+        //Print the contents of the input file
+        System.out.println(title);
+        System.out.println("Gross income:\t$" + grossIncome);
+        System.out.println("Tax rate:\t" + taxBracket + "%");
+        System.out.println("Tithe rate:\t" + tithe + "%");
+        System.out.println("Rent:\t\t$" + rent);
+        System.out.println("Food:\t\t$" + food + " per week");
+        System.out.println("Gas:\t\t$" + gas + " per week");
     }
 
-    public static void endProgram() {
-        //
+    /**Prints the cost of expenses for the month to the console
+     * 
+     * @param grossIncome
+     * @param taxBracket
+     * @param tithe
+     * @param rent
+     * @param food
+     * @param gas
+     */
+    public static void printExpenses(int grossIncome, int taxBracket, int tithe, int rent, int food, int gas) {
+        //Print expenses
+        double total = (double) grossIncome;
+        System.out.println();
+        System.out.println("Expenses");
+
+        //Print taxes
+        System.out.println("Tax: $" + total + " - $" + (Calculator.percentOf(total, (double) taxBracket)));
+        total -= (Calculator.percentOf((double) total, (double) taxBracket));
+        System.out.println("\t$" + total);
+
+        //Print Tithe
+        System.out.println("\nTithe: $" + total + " - $" + Calculator.percentOf((double) grossIncome, (double) tithe));
+        total -= Calculator.percentOf((double) grossIncome, (double) tithe);
+        System.out.println("\t$" + total);
+
+        //Print Rent
+        System.out.println("\nRent: $" + total + " - $" + rent);
+        total -= rent;
+        System.out.println("\t$" + total);
+
+        //Print Food
+        System.out.println("\nFood: $" + total + " - $" + (food * 4));
+        total -= food;
+        System.out.println("\t$" + total);
+
+        //Print Gas
+        System.out.println("\nGas: $" + total + " - $" + (gas * 4));
+        total -= gas;
+        System.out.println("\t$" + total);
     }
 
+    public static void printSubscriptions() {
+        //
+    }
+    
+    /**Closes sytem resources and prints runtime information.
+     * 
+     * @param file
+     * @param startTime
+     */
+    public static void endProgram(Scanner file, long startTime) {
+        //End of program, close resources
+        file.close();
+        long endTime = System.currentTimeMillis();
+        long runtime = endTime - startTime;
+        //runtime /= 1000; //Convert to seconds
+        System.out.println("\nRun time: " + runtime + " milliseconds");
+    }
+
+    /**Runs the main program.
+     * 
+     * @param args
+     */
     public static void run(String[] args) {
         //Timer
         long startTime = System.currentTimeMillis();
@@ -55,6 +133,7 @@ public class Main {
         int gas = -1;
 
         try {
+            //Read in values 
             title = file.nextLine();
             grossIncome = file.nextInt();
             taxBracket = file.nextInt();
@@ -68,51 +147,13 @@ public class Main {
             System.exit(0);
         }
 
-        //Print the contents of the input file
-        System.out.println(title);
-        System.out.println("Gross income:\t$" + grossIncome);
-        System.out.println("Tax rate:\t" + taxBracket + "%");
-        System.out.println("Tithe rate:\t" + tithe + "%");
-        System.out.println("Rent:\t\t$" + rent);
-        System.out.println("Food:\t\t$" + food + " per week");
-        System.out.println("Gas:\t\t$" + gas + " per week");
+        printFile(title, grossIncome, taxBracket, tithe, rent, food, gas);
 
-        //Print expenses
-        double total = (double) grossIncome;
-        System.out.println();
-        System.out.println("Expenses");
+        printExpenses(grossIncome, taxBracket, tithe, rent, food, gas);
 
-        //Print taxes
-        System.out.println("Tax: $" + total + " - $" + (Calculator.percentOf(total, (double) taxBracket)));
-        total -= (Calculator.percentOf((double) total, (double) taxBracket));
-        System.out.println("\t$" + total);
+        printSubscriptions();
 
-        //Print Tithe
-        System.out.println("\nTithe: $" + total + " - $" + Calculator.percentOf((double) grossIncome, (double) tithe));
-        total -= Calculator.percentOf((double) grossIncome, (double) tithe);
-        System.out.println("\t$" + total);
-
-        //Print Rent
-        System.out.println("\nRent: $" + total + " - $" + rent);
-        total -= rent;
-        System.out.println("\t$" + total);
-
-        //Print Food
-        System.out.println("\nFood: $" + total + " - $" + (food * 4));
-        total -= food;
-        System.out.println("\t$" + total);
-
-        //Print Gas
-        System.out.println("\nGas: $" + total + " - $" + (gas * 4));
-        total -= gas;
-        System.out.println("\t$" + total);
-        
-        //End of program, close resources
-        file.close();
-        long endTime = System.currentTimeMillis();
-        long runtime = endTime - startTime;
-        runtime /= 1000;
-        System.out.println("\nRun time: " + runtime + " seconds");
+        endProgram(file, startTime);
     }
     
     public static void main(String[] args) {
