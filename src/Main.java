406924c60e58287998d3bb2f30c3481927e35e28
daniel-grimm <danielsgrimm@hotmail.com>
2019-05-11 13:59:04 -0700

    Changing the structure of the application to be more dynamic.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 /**Automatically creates a budget from an input file.
  * 
+ * This program accepts a txt file as a data input. The file is expected to have to
+ * following structure:
+ * 
+ * Title
+ * Monthly_Income Integer_Value
+ * Tax_Bracket Tax_Bracket_Percent
+ * Tithe Percent_Value
+ * Savings_Percent Percent_Value
+ * Monthly_Expense Double_Value
+ * Monthly_Expense Double_Value
+ * ...
+ * end
+ * 
+ * For good design, put your re-occurring payments at the top of the list and
+ * one-off purchases at the bottom
+ * 
  * @author Daniel Grimm
- * @version 0.0
+ * @version 0.1
  */
 
 import java.io.File;                    //For file I/O
@@ -17,89 +33,39 @@ public class Main {
      * @param file
      */
     public static void displayInfo(Scanner file) throws IOException {
-        ArrayList<String> names = new ArrayList<String>();
-        ArrayList<Integer> costs = new ArrayList<Integer>();
+        ArrayList<String> expenses = new ArrayList<String>();   //Title of each expense
+        ArrayList<Double> costs = new ArrayList<Double>();      //Cost of each expense (Cost per month)
         String title = file.nextLine();
         String name = file.next();
-        Integer cost = new Integer(-1);
+        Double cost;
         while (!name.equals("end")) {
-            cost = new Integer(file.next());
-            names.add(name);
+            cost = new Double(file.next());
+            expenses.add(name);
             costs.add(cost);
             name = file.next();
         }
 
-        //Print contents of the file to the console
-        printFile(title, costs.get(0).intValue(), costs.get(1).intValue(), costs.get(2).intValue(), costs.get(3).intValue(), costs.get(4).intValue(), costs.get(5).intValue());
-        
-        //Print expenses to the console
-        printExpenses(costs.get(0).intValue(), costs.get(1).intValue(), costs.get(2).intValue(), costs.get(3).intValue(), costs.get(4).intValue(), costs.get(5).intValue());
+        printData(title, expenses, costs);
     }
 
-    /**Prints the content of the input file to the console.
+    /**This method prints the results of the file to the console.
+     * The first four lines are expected to be:
+     * Monthly Income
+     * Tax Percent
+     * Tithe Percent
+     * Investment/Savings Percent.
+     * 
+     * Deviation from this will result in erroneous output.
      * 
      * @param title
-     * @param grossIncome
-     * @param taxBracket
-     * @param tithe
-     * @param rent
-     * @param food
-     * @param gas
+     * @param expenses
+     * @param cost
      */
-    public static void printFile(String title, int grossIncome, int taxBracket, int tithe, int rent, int food, int gas) {
-        //Print the contents of the input file
-        System.out.println(title);
-        System.out.println("Gross income:\t$" + grossIncome);
-        System.out.println("Tax rate:\t" + taxBracket + "%");
-        System.out.println("Tithe rate:\t" + tithe + "%");
-        System.out.println("Rent:\t\t$" + rent);
-        System.out.println("Food:\t\t$" + food + " per week");
-        System.out.println("Gas:\t\t$" + gas + " per week");
-    }
+    public static void printData(String title, ArrayList<String> expenses, ArrayList<Double> cost) {
+        System.out.println("\n" + title);
+        System.out.println("\nGenerating Budget:\n");
 
-    /**Prints the cost of expenses for the month to the console
-     * 
-     * @param grossIncome
-     * @param taxBracket
-     * @param tithe
-     * @param rent
-     * @param food
-     * @param gas
-     */
-    public static void printExpenses(int grossIncome, int taxBracket, int tithe, int rent, int food, int gas) {
-        //Print expenses
-        double total = (double) grossIncome;
-        System.out.println();
-        System.out.println("Expenses");
-
-        //Print taxes
-        System.out.println("Tax: $" + total + " - $" + (Calculator.percentOf(total, (double) taxBracket)));
-        total -= (Calculator.percentOf((double) total, (double) taxBracket));
-        System.out.println("\t$" + total);
-
-        //Print Tithe
-        System.out.println("\nTithe: $" + total + " - $" + Calculator.percentOf((double) grossIncome, (double) tithe));
-        total -= Calculator.percentOf((double) grossIncome, (double) tithe);
-        System.out.println("\t$" + total);
-
-        //Print Rent
-        System.out.println("\nRent: $" + total + " - $" + rent);
-        total -= rent;
-        System.out.println("\t$" + total);
-
-        //Print Food
-        System.out.println("\nFood: $" + total + " - $" + (food * 4));
-        total -= food;
-        System.out.println("\t$" + total);
-
-        //Print Gas
-        System.out.println("\nGas: $" + total + " - $" + (gas * 4));
-        total -= gas;
-        System.out.println("\t$" + total);
-    }
-
-    public static void printSubscriptions() {
-        //
+        System.out.println(expenses.get(0) + ": $" + cost.get(0));
     }
     
     /**Closes sytem resources and prints runtime information.
